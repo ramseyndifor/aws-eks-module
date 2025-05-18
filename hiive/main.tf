0b7d2820b4fd9207c00e333e6b700b28a1630aea
Ramsey Ndifor <ramseyndifun@gmail.com>
2025-05-18 10:02:59 -0500

    updating variables

--- hiive/main.tf
+++ hiive/main.tf
@@ -1,6 +1,6 @@
 module "hiive-app" {
   source = "./module"
-  project_name = var.project_name
+  project_name = "hiive" #var.project_name
   region = var.region
   vpc_cidr = "10.0.0.0/16" #var.vpc_cidr
   container_image = var.container_image
